e5f194af38912e08b065630025c1d101f164db5d
thomasdreyer <tdreyer62@gmail.co.za>
2026-01-08 20:18:22 +0200

    updated resume

--- src/App.tsx
+++ src/App.tsx
@@ -23,11 +23,11 @@ export default function App() {
   return (
     <div className={lightMode === true ? 'AppLight' : 'AppDark'}>
       <div className="header">
-        <img src={me} className="image" alt="Thomas Dreyer" onClick={() => window.open('https://docs.google.com/document/d/1WLwzWn_-NsS7mZZIwO4rq5sIEBpnzObbSwE53-4taVU/edit?usp=sharing')} />
+        <img src={me} className="image" alt="Thomas Dreyer"  />
         {/* <span className="navButton" onClick={() => window.open('https://docs.google.com/document/d/1WLwzWn_-NsS7mZZIwO4rq5sIEBpnzObbSwE53-4taVU/edit?usp=sharing')} style={{ cursor: "pointer" }}><FaFileAlt />My Resume</span> */}
         <span className="navButton" onClick={() => storyRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' })}>My story</span>
         <span className="navButton" onClick={() => workRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' })}>My Work</span>
-        <span className="navButton" onClick={() => window.open('https://docs.google.com/document/d/1WLwzWn_-NsS7mZZIwO4rq5sIEBpnzObbSwE53-4taVU/edit?usp=sharing')} style={{ cursor: "pointer" }}><FaFileAlt />My Resume</span>
+        <span className="navButton" onClick={() => window.open('https://docs.google.com/document/d/e/2PACX-1vSFIYo14OSyU4vtUbRi-uP2pIrPrFGFXkublPTuC5q_4PZ0R2_EtQmTzxdtA2NtS5IV_UKICw6IfT0w/pub')} style={{ cursor: "pointer" }}><FaFileAlt />My Resume</span>
        
         {/* <span className="navButton" onClick={() => storyRef.current.scrollIntoView({ behavior: 'smooth', block: 'start' })}>My story</span> */}
        
